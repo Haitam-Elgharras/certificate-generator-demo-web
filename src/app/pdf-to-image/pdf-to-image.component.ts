4df5aa3656328fbf2e36893724b5712f7193ebf2
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-07-05 12:40:49 +0100

    Add popup for templates

---
+++ src/app/pdf-to-image/pdf-to-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-to-image',
+  templateUrl: './pdf-to-image.component.html',
+  styleUrl: './pdf-to-image.component.css'
+})
+export class PdfToImageComponent {
+
+}
---
+++ src/app/template-popup/template-popup.component.ts
@@ -0,0 +1,68 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CertificateTemplate } from '../models/certificateTemplate.model';
+import { CertificateTemplateService } from '../services/certificate-template.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-template-popup',
+  templateUrl: './template-popup.component.html',
+  styleUrls: ['./template-popup.component.css'],
+})
+export class TemplatePopupComponent {
+  @Output() closePopup = new EventEmitter<void>();
+
+  constructor(private certificateService: CertificateTemplateService, private router: Router) {}
+
+  templates: CertificateTemplate[] = [];
+
+  selectedTemplate: CertificateTemplate | null = null;
+
+  previewUrl: string = '';
+
+  previewTemplate(template:any) {
+    this.previewUrl = `http://localhost:8084/certificates/file/${template.id}`;
+
+    // open the preview in a new tab
+    window.open(this.previewUrl, '_blank');
+  }
+
+  ngOnInit() {
+    this.certificateService.getCertificateTemplates().subscribe({
+      next: (templates) => {
+        this.templates = templates;
+      },
+      error: (error) => {
+        console.error('Error fetching templates:', error);
+      },
+    });
+  }
+
+  deleteTemplate(id: number) {
+    // store the deleted template in a variable
+    const deletedTemplate = this.templates.find((template) => template.id === id);
+
+    // remove the template from the list
+    this.templates = this.templates.filter((template) => template.id !== id);
+
+    // send delete request to the server
+    this.certificateService.deleteCertificateTemplate(id).subscribe({
+      next: (response) => {
+        console.log('Template deleted:', response);
+      },
+      error: (error) => {
+        // if the delete request fails, add the template back to the list
+        if(deletedTemplate)
+          this.templates.push(deletedTemplate);
+      },
+    });
+  }
+
+  selectTemplate(template: CertificateTemplate) {
+    this.selectedTemplate = template;
+    this.closePopup.emit();
+  }
+
+  // previewTemplate(template: CertificateTemplate) {
+  //   console.log('Previewing template:', template);
+  // }
+}
